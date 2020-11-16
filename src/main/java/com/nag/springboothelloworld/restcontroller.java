e95254d972a85c216c4fc04027e7940159e4dc4c
nag <sailunag18@gmail.com>
2020-11-16 18:35:40 +0530

    Created a new branch

--- src/main/java/com/nag/springboothelloworld/restcontroller.java
+++ src/main/java/com/nag/springboothelloworld/restcontroller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class restcontroller {
     @GetMapping("/greeting")
     public String greeting(){
-    return "Hello world";
+    return "Hello world Security";
 }
 
 }
