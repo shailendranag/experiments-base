7eea9d3ce901f0fca655db951ae1cab0e3786d10
nag <sailunag18@gmail.com>
2020-11-16 12:34:07 +0530

    Initial Commit

---
+++ src/main/java/com/nag/springboothelloworld/restcontroller.java
@@ -0,0 +1,14 @@
+package com.nag.springboothelloworld;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class restcontroller {
+    @GetMapping("/greeting")
+    public String greeting(){
+    return "Hello world";
+}
+
+}
---
+++ src/test/java/com/nag/springboothelloworld/SpringBootHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nag.springboothelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
