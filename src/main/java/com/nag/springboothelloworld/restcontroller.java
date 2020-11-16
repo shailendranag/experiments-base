b9df10043901c6724125ee8ca9806cd44c0accd7
nag <sailunag18@gmail.com>
2020-11-16 19:17:46 +0530

    First Draft With SecurityConfiguration

--- src/main/java/com/nag/springboothelloworld/restcontroller.java
+++ src/main/java/com/nag/springboothelloworld/restcontroller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class restcontroller {
-    @GetMapping("/greeting")
+    @GetMapping("/")
     public String greeting(){
     return "Hello world Security";
 }
