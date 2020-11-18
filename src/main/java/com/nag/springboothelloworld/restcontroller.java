52a381bcdada4e539cbdb91ad2a28d0f2d0f5a60
nag <sailunag18@gmail.com>
2020-11-18 11:22:59 +0530

    Updated teh auth part

--- src/main/java/com/nag/springboothelloworld/restcontroller.java
+++ src/main/java/com/nag/springboothelloworld/restcontroller.java
@@ -6,11 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class restcontroller {
-    @GetMapping("/")
-    public String greeting(){
-    return "Hello world Security";
-}
-
     @GetMapping("/admin")
     public String admin(){ return "Hello World from Admin";}
 
